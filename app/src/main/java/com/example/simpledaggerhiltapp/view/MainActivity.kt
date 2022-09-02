4453cc07f01bd3865c50998da9499933bac47f38
savelgmx <savel@gmx.com>
2022-09-02 10:51:32 +0700

    fixed Builder Error almost complete

--- app/src/main/java/com/example/simpledaggerhiltapp/view/MainActivity.kt
+++ app/src/main/java/com/example/simpledaggerhiltapp/view/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.simpledaggerhiltapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.simpledaggerhiltapp.R
 import com.example.simpledaggerhiltapp.viewmodel.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.flow.collect
+
 
 
 @AndroidEntryPoint
@@ -14,5 +19,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.conversion.collect { value: WeatherViewModel.WeatherEvent ->
+                when(value){
+                    is WeatherViewModel.WeatherEvent.Success ->{
+                        progressBar.visibility = View.GONE
+                        openWeather.setText(value.resultText)
+                    }
+                    is WeatherViewModel.WeatherEvent.Failure -> {
+                        progressBar.visibility = View.GONE
+                        openWeather.setText(value.errorText)
+                    }
+                    is WeatherViewModel.WeatherEvent.Loading -> {
+                        progressBar.visibility = View.VISIBLE
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
     }
 }
